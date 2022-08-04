862e5151cb4231f9f46d2ae176a14ad482a542a2
Duncan Forster <duncan.forster@mail.utoronto.ca>
2022-08-04 15:38:02 -0400

    Delete activate.sh

--- activate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export PATH=/pkgs/anaconda3/bin:$PATH
-export PATH=/h/forsterd/.poetry/bin:$PATH
-export PYTHONPATH=$HOME/.conda/envs/bionic-evals-original:$PYTHONPATH
-source activate bionic-evals-original
